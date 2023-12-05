57a7c8fb3790f0d07f2ee25de13d0c1c98e33d50
Max Mackie <maxmackie13@gmail.com>
2023-12-05 16:44:18 +0000

    completed challenge 1 day 5

--- code.js
+++ code.js
@@ -11,43 +11,104 @@ const fetchData = async () => {
 };
 
 const run = async () => {
-    const data = await fetchData();
     // Challenge code below:
-    const cards = data.trim().split('\n')
-    const processedCards = cards.map(card => {
-        const match = card.match(/Card\s*(\d+)/)
-        const id = match ? parseInt(match[1], 10) : null
-        const parts = card.split("|");
-        const winningNums = parts[0].split(":")[1].match(/\d+/g).map(Number)
-        const myNums = parts[1].match(/\d+/g).map(Number)
-        const matchingNums = myNums.filter(num => winningNums.includes(num))
+    const splitArray = (inputArray, delimiter) => {
+        let result = []
+        let currentSegment = []
+        inputArray.forEach( element => {
+            if(element === delimiter){
+                if(currentSegment.length){
+                    result.push(currentSegment)
+                    currentSegment = []
+                }
+            }else{
+                currentSegment.push(element)
+            }
+        });
+        if(currentSegment.length){
+            result.push(currentSegment)
+        }
+        return result
+    }
 
-        // const points = matchingNums.length > 0 ? 2**(matchingNums.length - 1) : 0
-        // return points
-        return {[id]: matchingNums.length}
-    })
-    console.log(processedCards)
-    const processedCardsObj = Object.assign({}, ...processedCards)
-    let count = 0;
-    while(processedCards.length > 0){
-    // for(let i=0; i<10; i++){
-        const card = processedCards.shift()
-        const id = parseInt(Object.keys(card)[0], 10)
-        const matchingNums = card[id]
-        if(matchingNums == 0){
-            count++
-        }else{
-            count++
-            for(let i=matchingNums; i>0; i--){
-                const newId = id+i
-                const newObj = {[newId]: processedCardsObj[newId]}
-                processedCards.unshift(newObj)
+    const data = await fetchData();
+    const mapData = data.trim().split('\n')
+    const mapsRanges = splitArray(mapData, "")
+
+    let seeds = mapsRanges.shift()[0].match(/\d+/g).map(Number)
+    let mapObj = {};
+    for(let range of mapsRanges){
+        const name = range.shift()
+        let map = range.map(row => {
+            return row.match(/\d+/g).map(Number)
+        })
+        mapObj[name] = map
+    }
+    let locations = []
+
+    
+    seeds.forEach(seed => {
+        let soil
+        let fertilizer
+        let water
+        let light
+        let temperature
+        let humidity
+        let location
+        for(let row of mapObj['seed-to-soil map:']){     
+            if(seed >= row[1] && seed <= (row[1] + row[2])){
+                soil = seed + (row[0] - row[1])
             }
         }
-    }
-    
-    console.log(count)
-    return count
+        if(!soil) soil = seed
+
+        for(let row of mapObj['soil-to-fertilizer map:']){
+            if(soil >= row[1] && soil <= (row[1] + row[2])){
+                fertilizer = soil + (row[0] - row[1])
+            }
+        }
+        if(!fertilizer) fertilizer = soil
+
+        for(let row of mapObj['fertilizer-to-water map:']){
+            if(fertilizer >= row[1] && fertilizer <= (row[1] + row[2])){
+                water = fertilizer + (row[0] - row[1])
+            }
+        }
+        if(!water) water = fertilizer
+        
+        for(let row of mapObj['water-to-light map:']){
+            if(water >= row[1] && water <= (row[1] + row[2])){
+                light = water + (row[0] - row[1])
+            }
+        }
+        if(!light) light = water
+
+        for(let row of mapObj['light-to-temperature map:']){
+            if(light >= row[1] && light <= (row[1] + row[2])){
+                temperature = light + (row[0] - row[1])
+            }
+        }
+        if(!temperature) temperature = light
+        
+        for(let row of mapObj['temperature-to-humidity map:']){
+            if(temperature >= row[1] && temperature <= (row[1] + row[2])){
+                humidity = temperature + (row[0] - row[1])
+            }
+        }
+        if(!humidity) humidity = temperature
+        
+        for(let row of mapObj['humidity-to-location map:']){
+            if(humidity >= row[1] && humidity <= (row[1] + row[2])){
+                location = humidity + (row[0] - row[1])
+            }
+        }
+        if(!location) location = humidity
+        locations.push(location) 
+    })
+
+    console.log(Math.min(...locations))
+    return 
 }
+
 
 run();
