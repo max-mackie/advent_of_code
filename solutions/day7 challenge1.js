cfa8519ab16aef522a8ab1ef692d6c16c7a19427
Max Mackie <maxmackie13@gmail.com>
2023-12-07 14:27:57 +0000

    completed day 7 challenge 1

--- solutions/day7 challenge1.js
+++ solutions/day7 challenge1.js
@@ -144,37 +144,3 @@ const part1 = async () => {
 
 part1();
 // part2();
-
-
-async function  solve1 () {
-    // prettier-ignore
-    const input = await fetchData();
-    const cards = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
-  
-    const lines = input.split('\n').filter(Boolean);
-    for (let i = 0; i < lines.length; i++) {
-      let [hand, bid] = lines[i].split(' ');
-      hand = hand.split('').map((card) => cards.indexOf(card).toString(16));
-      bid = +bid;
-
-      const map = {};
-      for (const card of hand) {
-        map[card] = (map[card] ?? 0) + 1;
-      }
-      const counts = Object.values(map).sort((a, b) => b - a);
-  
-      lines[i] = [[counts[0], counts[1] ?? 0, ...hand].join(), bid];
-    }
-  
-    lines.sort(([a], [b]) => a.localeCompare(b));
-  
-    let sum = 0;
-    for (let i = 0; i < lines.length; i++) {
-    //   console.log(i, lines[i])
-      sum += lines[i][1] * (i + 1);
-    }
-    console.log(sum);
-  }
-    
-
-  solve1();
